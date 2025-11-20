e7643c02dcd8d1fdee4f01f502a4b2eac4b510d1
PedroVhGit <pedrovh040110@gmail.com>
2025-11-20 16:49:06 -0500

    Bump version to 2.0.5-dev and update HTTP client to use insecure TLS configuration

--- polling/nf_configuration.go
+++ polling/nf_configuration.go
@@ -7,6 +7,7 @@ package polling
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -37,7 +38,12 @@ type nfConfigPoller struct {
 func StartPollingService(ctx context.Context, webuiUri string, registrationChannel, contextUpdateChannel chan []nfConfigApi.AccessAndMobility) {
 	poller := nfConfigPoller{
 		currentAccessAndMobilityConfig: []nfConfigApi.AccessAndMobility{},
-		client:                         &http.Client{Timeout: initialPollingInterval},
+		client: &http.Client{
+			Timeout: initialPollingInterval,
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		},
 	}
 	interval := initialPollingInterval
 	pollingEndpoint := webuiUri + pollingPath
